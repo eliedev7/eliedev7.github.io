618bb59adc3853bc13c89f868666ba229df6c214
IRA Eliezer <95850928+moniraelizer@users.noreply.github.com>
2022-12-27 05:53:34 +0100

    Première fonctions

--- main.js
+++ main.js
@@ -12,6 +12,62 @@ init();  // on utilse ce bloc pour initialiser le jeu simplement. on on l'appele
 // de générer d'abord un nombre aléatoire entre 1 et 6 (à l'image du dé). Ensuite, on essaira d'afficger le résultat en image dans le jeu et
 // l'ajouter dans le roundScore du joueur actif. on fait de sorte que si le joueur jour "1" il passe son tour.
 
+// Fonction "ROLL DICE"
+document.querySelector("#roll-the-dice").addEventListener("click", function () {
+    if (gaming) {
+        // 1. Création d'un nombre aléatoire
+        let dice = Math.floor(Math.random() * 6) + 1;
 
+        // 2. l'affichage du résultat 
+        let diceDOM = document.querySelector(".dice");
+        diceDOM.style.display = "block";
+        diceDOM.src = "./img/die-" + dice + ".svg";
+
+        // 3. On met à jour le résultat roundScore qui est la somme des nombre issu de la variable dice
+        if (dice !== 1) {
+            roundScore += dice;
+            document.getElementById(
+                "round-" + activePlayer
+            ).textContent = roundScore;
+        } else {
+            // Pssage de tour
+            nextPlayer();
+            alert("Désolé vous avez tapé le nombre 1, vous passez votre tour !");
+        }
+    }
+});
+
+/* Le boutton Hold permet de passer le tour. cela peut se faire sous deux conditions :
+   - si le joueur joue "1". Mais cela implique la supression immédiate de son roundScore
+   - si il clique volontairement sur le boutton "HOLD"
+*/
+// Fonction "HOLD"
+document.querySelector("#take-a-turn").addEventListener("click", function () {
+    if (gaming) {
+        // On ajoute le score temporaire au score globale
+        scores[activePlayer] += roundScore;
+
+        document.querySelector("#score-" + activePlayer).textContent =
+            scores[activePlayer];
+
+        // On vérifie la possible victoire d'un joueur
+        if (scores[activePlayer] >= 100) {
+            document.querySelector("#name-" + activePlayer).textContent = "Winner!";
+            document.querySelector(".dice").style.dispaly = "none";
+
+            document
+                .querySelector(".player-" + activePlayer + "-panel")
+                .classList.add("winner");
+            document
+                .querySelector(".player-" + activePlayer + "-panel")
+                .classList.remove("active");
+
+            gaming = false;
+        } else {
+            // Next player
+            nextPlayer();
+        }
+    }
+});
 
 
