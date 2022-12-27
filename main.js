526222a902c32a73dbd28b5f7245ad6346808ae6
IRA Eliezer <95850928+moniraelizer@users.noreply.github.com>
2022-12-27 06:10:35 +0100

    j'ai été obligé de suprimer les fichiers pour un soucis au niveau du dé que je n'arrivais pas à comprendre

--- main.js
+++ main.js
@@ -1,56 +1,46 @@
-//On commence par définir des variables essentielles pour le jeu.
-// (scores) ==> est le score global d'un joueur durant le jeu. Le joueur gagne la partie quand il atteint  100 comme score
-// (roundscore) ==> est le score temporaire d'un joueur. il a la possibilité de déverser son roundscore dans le score global. 
-// (activePlayer) ==> est une instance pour travailler sur le joueur en activité. c'est à dire celui qui joue. 
-// gaming est un booléen simple qui nous permettra d'initialiser le jeu.
 let scores, roundScore, activePlayer, gaming;
 
-init();  // on utilse ce bloc pour initialiser le jeu simplement. on on l'appelera plus tard
+init();
 
-
-// Pour créer le jeu on commencera par créer le boutton de lancement du dé. Pourt ce fait on va  créer une fonction. cette fonction permettra
-// de générer d'abord un nombre aléatoire entre 1 et 6 (à l'image du dé). Ensuite, on essaira d'afficger le résultat en image dans le jeu et
-// l'ajouter dans le roundScore du joueur actif. on fait de sorte que si le joueur jour "1" il passe son tour.
-
-// Fonction "ROLL DICE"
+// ROLL button
 document.querySelector("#roll-the-dice").addEventListener("click", function () {
     if (gaming) {
-        // 1. Création d'un nombre aléatoire
+        // 1. Random number
         let dice = Math.floor(Math.random() * 6) + 1;
 
-        // 2. l'affichage du résultat 
+        // 2. Display the result
         let diceDOM = document.querySelector(".dice");
         diceDOM.style.display = "block";
         diceDOM.src = "./img/die-" + dice + ".svg";
 
-        // 3. On met à jour le résultat roundScore qui est la somme des nombre issu de la variable dice
+        // 3. Update the round score IF the rolled number was NOT a 1
         if (dice !== 1) {
+            // Add score
             roundScore += dice;
             document.getElementById(
                 "round-" + activePlayer
             ).textContent = roundScore;
         } else {
-            // Pssage de tour
+            // Next player
             nextPlayer();
             alert("Désolé vous avez tapé le nombre 1, vous passez votre tour !");
         }
     }
 });
 
-/* Le boutton Hold permet de passer le tour. cela peut se faire sous deux conditions :
-   - si le joueur joue "1". Mais cela implique la supression immédiate de son roundScore
-   - si il clique volontairement sur le boutton "HOLD"
-*/
-// Fonction "HOLD"
+
+
+// HOLD button
 document.querySelector("#take-a-turn").addEventListener("click", function () {
     if (gaming) {
-        // On ajoute le score temporaire au score globale
+        // Add round score to GLOBAL score
         scores[activePlayer] += roundScore;
 
+        // Update the UI
         document.querySelector("#score-" + activePlayer).textContent =
             scores[activePlayer];
 
-        // On vérifie la possible victoire d'un joueur
+        // Check if player won the game
         if (scores[activePlayer] >= 100) {
             document.querySelector("#name-" + activePlayer).textContent = "Winner!";
             document.querySelector(".dice").style.dispaly = "none";
@@ -64,14 +54,12 @@ document.querySelector("#take-a-turn").addEventListener("click", function () {
 
             gaming = false;
         } else {
-
+            // Next player
             nextPlayer();
         }
     }
 });
 
-
-// fonction pour passer le tour
 function nextPlayer() {
     roundScore = 0;
     activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
@@ -83,7 +71,7 @@ function nextPlayer() {
     document.querySelector(".dice").style.display = "none";
 }
 
-// Fonction pour initialiser le jeu
+// NEW-GAME button
 document.querySelector(".btn-new").addEventListener("click", init);
 
 function init() {
@@ -109,5 +97,3 @@ function init() {
     document.querySelector(".player-1-panel").classList.remove("active");
     document.querySelector(".player-0-panel").classList.add("active");
 }
-
-
